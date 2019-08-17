726f25c9e2e6974fe2c0d15dbcf03c0dc5e781b6
hc12345678 <1737957810@qq.com>
2019-08-17 14:25:41 +0800

    xiong

---
+++ src/components/loginAndReg.vue
@@ -0,0 +1,256 @@
+<template>
+
+	<div>
+		<div class="mui-content" style="background-color: inherit">
+			<div class="mui-content-padded" style="text-indent: 5%;margin-top: 100px;">
+				<h2>您好，请登录</h2>
+			</div>
+
+
+
+			<form class="mui-input-group myform">
+				<div class="mui-input-row" style="height: 80px;">
+					<label style="font-size: 40px;"><span class="mui-icon mui-icon-phone"></span></label>
+					<input v-validate="'required|mobile'" v-model="phoneNum" style="height: 80px;" type="text" placeholder="请输入手机号">
+				</div>
+				<div class="mui-input-row" style="height: 80px;">
+					<label style="font-size: 40px;"><span class="mui-icon mui-icon-locked"></span></label>
+					<input v-validate="''" v-model="textCode" style="height: 80px;" type="text" placeholder="请输入验证码">
+					<span @click="sendTextCode" :style="{'color':sendTextCodeBtnIsDisabled?'#9ba9b7':'#007aff'}" class="yzmBtn">{{sendTextCodeTip}}</span>
+				</div>
+			</form>
+
+			<button @click="regOrLoginWithPhoneCode" type="button" class="mui-btn mui-btn-block circleBtn">登录/注册</button>
+
+			<div class="mui-row changeLoginMethod">
+				账号密码登录
+			</div>
+
+
+
+		</div>
+
+		<nav class="mui-bar mui-bar-tab" style="box-shadow: none;background-color: inherit;text-align: center;font-size: 14px;color: grey;">
+			如无法登录，请点击<span style="font-weight: bold;color: #000000;">账号申诉</span>申请客服协助
+		</nav>
+
+
+
+	</div>
+
+</template>
+
+<script>
+	export default {
+		name: 'loginAndReg',
+		data() {
+			return {
+				phoneNum: '', // 手机号
+				textCode: '', // 短信验证码
+				sendTextCodeTip: '发送验证码',
+				phoneToken: '',
+				sendTextCodeBtnIsDisabled: false,
+				searchData: {
+					"model": {
+
+					},
+					"orderParams": [
+
+					],
+					"pageNum": 1,
+					"pageSize": 10
+				}
+			}
+		},
+		methods: {
+			toLogin: function() {
+				this.$router.push({
+					path: '/login'
+				})
+			},
+			sendTextCode: function() {
+				if (this.sendTextCodeBtnIsDisabled) {
+
+					return
+				}
+
+				this.$validator.validate().then(async (valid) => {
+					if (!valid) {
+						// do stuff if not valid.
+						this.$mui.toast('手机号或验证码有误')
+						return
+					} else {
+						var that = this
+						var getPhoneTokenData = {
+							"phone": that.phoneNum,
+							"templateId": 1 // 1 是发送普通手机验证码
+						}
+						var res = await that.$http.post('/message-server/textMessage/getPhoneToken', getPhoneTokenData)
+						if (res.data.code == 200) {
+							var token = res.data.content
+							that.phoneToken = token
+							var res = await that.$http('/message-server/textMessage/sendCodeByPhone/' + token)
+							if (res.data.code == 200) {
+								// 如果发送验证码成功 禁用发送按钮 改变提示内容  60秒计时倒数
+								that.$mui.toast('验证码已发送至您的手机')
+								that.sendTextCodeBtnIsDisabled = true
+								var _that = that
+								var num = 60
+								_that.sendTextCodeTip = '已发送(' + num + ')'
+								var interval = setInterval(function() {
+									num--
+									_that.sendTextCodeTip = '已发送(' + num + ')'
+									if (num == 0) {
+										clearInterval(interval)
+										_that.sendTextCodeBtnIsDisabled = false
+										_that.sendTextCodeTip = '重新发送'
+									}
+								}, 1000)
+							} else {
+								that.$mui.toast('验证码发送失败')
+							}
+						} else {
+							that.$mui.toast('验证码发送失败')
+						}
+					}
+				})
+			},
+			regOrLoginWithPhoneCode: function() {
+				var that = this
+				if (this.textCode == '') {
+					this.$mui.toast('验证码不能为空')
+					return
+				}
+
+				if (that.phoneToken == '') {
+					this.$mui.toast('验证码错误,请重试')
+					return
+				}
+
+				this.$validator.validate().then(async (valid) => {
+					var res = await that.$http('/message-server/textMessage/verifyCodeByPhone/' + that.phoneToken + '/' + that.textCode)
+					if (res.data.code == 200) {
+						// 手机验证成功  执行登录和注册操作
+						// 先判断是否注册过
+						that.searchData.model.phoneNum = that.phoneNum
+						var res = await that.$http.post('/user-server/getAllUser', that.searchData)
+						if (res.data.code == 200 && res.data.content.list.length == 0) {
+							// 执行注册操作
+							that.regUser()
+						} else {
+							// 执行登录操作
+							that.loginUser()
+						}
+					} else {
+						this.$mui.toast('验证失败,请重试')
+					}
+				})
+			},
+			regUser: function() {
+				var that = this
+				var userInfo = {}
+				userInfo.phoneNum = this.phoneNum
+				userInfo.password = '123456'
+				userInfo.userName = this.phoneNum
+
+				// this.userInfo.phoneNum = this.phoneNum
+				// this.userInfo.password = '123456'
+				// this.userInfo.userName = this.phoneNum
+				this.$http.post('/user-server/addUser', userInfo).then(res => {
+					if (res.data.code == 200) {
+						// alert('test')
+						that.$setCookie('userId', res.data.content.id, 24 * 60 * 60)
+						// that.$setCookie('companyId', res.data.content.companyId, 30 * 60)
+						// that.$setCookie('schoolId', res.data.content.schoolId, 30 * 60)
+						// that.$setCookie('realName', res.data.content.realName, 30 * 60)
+						// that.$setCookie('roleId', res.data.content.roleId, 30 * 60)
+						// that.$setCookie('headImg', res.data.content.headImg, 30 * 60)
+						// that.$setCookie('nickName', res.data.content.nickName, 30 * 60)
+						that.$mui.toast('注册成功，马上跳转应用首页...')
+						setTimeout(function() {
+							that.$router.push('/')
+						}, 2000)
+
+					} else {
+						that.$mui.toast('注册失败')
+					}
+				}).then(err => {
+					that.$log(err)
+					// that.$warnMsg('注册失败')
+				})
+			},
+			loginUser: function() {
+				var searchData = {
+					"model": {
+						phoneNum: this.phoneNum
+					},
+					"orderParams": [
+
+					],
+					"pageNum": 1,
+					"pageSize": 10
+				}
+				this.$http.post('/user-server/getAllUser', searchData).then(res => {
+					if (res.data.code == 200 && res.data.content.list.length == 1) {
+						this.$setCookie('userId', res.data.content.list[0].id, 24 * 60 * 60)
+						this.$router.push('/')
+					} else {
+						this.$mui.toast('登录失败')
+					}
+				})
+			}
+		},
+		mounted: function() {
+
+		}
+
+	}
+</script>
+
+<style scoped="scoped">
+	.yzmBtn {
+		position: absolute;
+		right: 14px;
+		top: 30px;
+		color: #007aff;
+	}
+
+	.myform {
+		background-color: inherit;
+		width: 90%;
+		margin: 0px auto;
+		margin-top: 50px;
+	}
+
+	.circleBtn {
+		border-radius: 100px;
+		margin-top: 30px;
+		border: 0.5px solid grey;
+		background-color: inherit;
+		width: 90%;
+		margin: 20px auto;
+		margin-top: 50px;
+	}
+
+	.circleBtn:nth-of-type(1) {
+		color: white;
+		border: none;
+		/* 渐变色 */
+		background: -webkit-linear-gradient(left, #01DBE7, #018AFB);
+		/* Safari 5.1 - 6.0 */
+		background: -o-linear-gradient(right, #01DBE7, 018AFB);
+		/* Opera 11.1 - 12.0 */
+		background: -moz-linear-gradient(right, #01DBE7, #018AFB);
+		/* Firefox 3.6 - 15 */
+		background: linear-gradient(to right, #01DBE7, #018AFB);
+		/* 标准的语法（必须放在最后） */
+	}
+
+	.changeLoginMethod {
+		font-size: 14px;
+		color: black;
+		text-align: right;
+		padding-right: 7%;
+		font-weight: bold;
+	}
+</style>
---
+++ src/components/regularPractice.vue
@@ -0,0 +1,456 @@
+<template>
+
+	<div>
+
+		<div id="question" class="mui-content">
+			<header class="header">
+				<a @click="toIndex" href="javascript:;"><span class="mui-icon mui-icon-arrowthinleft"></span></a>
+				<h5>顺序练习({{questionJobTypeSelectedName}})</h5>
+			</header>
+
+			<div class="mui-content-padded">
+				<span v-if="currentQuestion.questionTypeId==1" class="mui-badge mui-badge-blue">选择题</span>
+				<span v-if="currentQuestion.questionTypeId==2" class="mui-badge mui-badge-green">判断题</span>
+				<span v-if="currentQuestion.questionTypeId==3" class="mui-badge mui-badge-warning">多选题</span>
+				{{currentQuestion.question}}
+				<img v-if="currentQuestion.questionImg1!=''&&currentQuestion.questionImg1!=null" data-preview-src="" width="100%"
+				 :src="currentQuestion.questionImg1" alt="">
+			</div>
+			<ul class="options" id="options">
+				<li @click="chooseOption('A',$event)">
+					<a href="javascript:;">
+						<span id="A" class="circleSpan">A</span>
+						{{currentQuestion.optionA}}
+					</a>
+				</li>
+				<li @click="chooseOption('B',$event)">
+					<a href="javascript:;">
+						<span id="B" class="circleSpan">B</span>
+						{{currentQuestion.optionB}}
+					</a>
+				</li>
+				<li @click="chooseOption('C',$event)" v-show="currentQuestion.questionTypeId==1||currentQuestion.questionTypeId==3">
+					<a href="javascript:;">
+						<span id="C" class="circleSpan">C</span>
+						{{currentQuestion.optionC}}
+					</a>
+				</li>
+				<li @click="chooseOption('D',$event)" v-show="currentQuestion.questionTypeId==1||currentQuestion.questionTypeId==3">
+					<a href="javascript:;">
+						<span id="D" class="circleSpan">D</span>
+						{{currentQuestion.optionD}}
+					</a>
+				</li>
+			</ul>
+
+			<div v-if="isAnswered">
+				<div class="mui-content-padded">
+					<h4>试题详解</h4>
+				</div>
+
+				<div class="mui-card">
+					<div class="mui-card-content">
+						<div class="mui-card-content-inner mui-table-view-cell">
+							<div class="mui-content-padded my_answer">
+								<h4>答案：{{currentQuestion.rightOption}}</h4>
+								<div class="right">报错 <span>考朋友</span></div>
+							</div>
+							<div>
+								<strong>题目解析：</strong>{{currentQuestion.explaination}}
+							</div>
+							<img v-if="currentQuestion.explainationImg1!=''&&currentQuestion.explainationImg1!=null" width="100%" style="margin-bottom: 10px"
+							 :src="currentQuestion.explainationImg1">
+
+							<div>
+								<strong>详细解释：</strong>{{currentQuestion.fullExplaination}}
+							</div>
+							<img v-if="currentQuestion.fullExplainationImg1!=''&&currentQuestion.fullExplainationImg1!=null" width="100%"
+							 style="margin-bottom: 10px" :src="currentQuestion.fullExplainationImg1">
+
+						</div>
+					</div>
+				</div>
+
+				<div class="mui-content-padded">
+					<h4>题目点评(开发中)</h4>
+				</div>
+				<ul class="mui-table-view" style="margin-bottom: 40px;">
+					<li class="mui-table-view-cell mui-media">
+						<a href="javascript:;">
+							<img class="mui-media-object mui-pull-left" src="http:\/\/placehold.it\/40x40">
+							<div class="mui-media-body">
+								<p class="my_comment">
+									用户12839 &nbsp;&nbsp;
+									<span class="mui-icon mui-icon-chat"></span>
+									<span class="my_up mui-icon ">999+</span>
+								</p>
+								<p style="color: #000;" class="mui-ellipsis">这道题实在是太难了太难了</p>
+							</div>
+						</a>
+					</li>
+					<li class="mui-table-view-cell mui-media">
+						<a href="javascript:;">
+							<img class="mui-media-object mui-pull-left" src="http:\/\/placehold.it\/40x40">
+							<div class="mui-media-body">
+								<p class="my_comment">
+									用户12839 &nbsp;&nbsp;
+									<span class="mui-icon mui-icon-chat"></span>
+									<span class="my_up mui-icon ">999+</span>
+								</p>
+								<p style="color: #000;" class="mui-ellipsis">这道题实在是太难了太难了</p>
+							</div>
+						</a>
+					</li>
+					<li class="mui-table-view-cell mui-media">
+						<a href="javascript:;">
+							<img class="mui-media-object mui-pull-left" src="http:\/\/placehold.it\/40x40">
+							<div class="mui-media-body">
+								<p class="my_comment">
+									用户12839 &nbsp;&nbsp;
+									<span class="mui-icon mui-icon-chat"></span>
+									<span class="my_up mui-icon ">999+</span>
+								</p>
+								<p style="color: #000;" class="mui-ellipsis">这道题实在是太难了太难了</p>
+							</div>
+						</a>
+					</li>
+
+				</ul>
+			</div>
+
+
+			<nav class="my_form">
+
+				<a @click="clearLocalAnswerLog" href="javascript:;"><span style="font-size: 14px;" class="mui-icon mui-icon-bars"></span>清除答题数据</a>
+				<div class="my_submit">
+					<span class="mui-icon mui-icon-checkmarkempty">{{rightCount}}</span>
+					<span class="mui-icon mui-icon-closeempty">{{falseCount}}</span>
+					<span><strong>{{questionIndex}}</strong>/{{questionCount}}</span>
+				</div>
+			</nav>
+
+		</div>
+		<div v-show="phoneModel=='Android'" class="androidCtlBtn" style="position: fixed;bottom: 100px;width: 100%;">
+			<span id="preBtn" @click="preQuestion" class="mui-icon mui-icon-arrowthinleft leftBtn" style="z-index: 1000;font-size: 50px;border: 1px solid #0db80d87;border-radius: 35px;float: left;margin-left: 10px;background-color: #3ed01e;color: white;opacity: 0.5;"></span>
+			<span id="nextBtn" @click="nextQuestion" class="mui-icon mui-icon-arrowthinright rightBtn" style="z-index: 1000;font-size: 50px;border: 1px solid #0db80d87;border-radius: 35px;float: right;margin-right: 10px;background-color: #3ed01e;color: white;opacity: 0.5;"></span>
+		</div>
+	</div>
+
+</template>
+
+<script>
+	export default {
+		name: 'regularPractice',
+		data() {
+			return {
+				questionJobTypeSelectedId: 1,
+				questionJobTypeSelectedName: '',
+				// questionTypeId:1,
+				questionIndex: 0,
+				questionCount: 0,
+				searchData: {
+					"model": {
+						isChecked: 2
+					},
+					"orderParams": [
+						'createDate asc'
+					],
+					"pageNum": 1,
+					"pageSize": 1
+				},
+				currentQuestion: {},
+				isAnswered: false, // 当前试题是否已经被回答
+				answerQuestionIds: [],
+				rightCount: 0,
+				falseCount: 0,
+				changeSpan: null,
+				phoneModel: 'others',
+				tempAnswer:'',
+				localAnswerLog:[]   //  浏览器本地记录答题数据
+			}
+		},
+		methods: {
+			toLogin: function() {
+				this.$router.push({
+					path: '/login'
+				})
+			},
+			clearLocalAnswerLog:function(){
+				var tempArr = [] // 临时存放其他岗位的答题记录
+				var answerArr = JSON.parse(localStorage.getItem('localAnswerLog'))||[];
+				for (var i = 0; i < answerArr.length; i++) {
+					if(answerArr[i].questionTypeId != this.questionJobTypeSelectedId){
+						tempArr.push(answerArr[i])
+					}
+				}
+				localStorage.removeItem('localAnswerLog')
+				localStorage.setItem('localAnswerLog',JSON.stringify(tempArr))
+				this.falseCount = 0
+				this.rightCount = 0
+				this.questionIndex = 1
+				this.$mui.toast('答题记录清除成功')
+				this.$router.push('/')
+				
+			},
+			getQuestion: async function() {
+				var that = this
+				//根据索引查询出下一题的数据
+				// that.searchData.pageSize = that.questionIndex
+				// that.searchData.pageNum = 2
+				// that.searchData.questionJobTypeId = that.questionJobTypeSelectedId
+				// console.log(that.searchData.questionJobTypeId)
+				var searchData2 = {
+					"model": {
+						isChecked: 2,
+						questionJobTypeId: that.questionJobTypeSelectedId
+					},
+					"orderParams": [
+				
+					],
+					"pageNum": 2,
+					"pageSize": that.questionIndex
+				}
+				var res = await this.$http.post('/msbd/getAllQuestion', searchData2)
+
+				var currentQuestion = res.data.content.list[0]
+				//拿着该题的id 去服务端查询用户答题表中查询 如果已经答过了 索引加1 递归调用
+				var searchData = {
+					"model": {
+						answerUserId: this.$getCookie('userId'),
+						questionId: currentQuestion.id
+					},
+					"orderParams": [
+
+					],
+					"pageNum": 1,
+					"pageSize": 100
+				}
+				// 服务器端查询用户是否回答过本题
+				var res2 = await this.$http.post('/msbd/getAllUseranserquestion', searchData)
+				// console.log(res2)
+				if (res2.data.content.list.length == 0) {  //  如果用户没回答过这题
+					that.currentQuestion = res.data.content.list[0]
+					// 查询本地答题记录
+					for (var i = 0; i < this.localAnswerLog.length; i++) {
+						if(this.localAnswerLog[i].questionId==that.currentQuestion.id){
+							// 说明该题已经答过了
+							this.isAnswered = true
+							// 显示正确答案和当时答错的答案
+							var userAnswerArr = this.localAnswerLog[i].answer.split('')  // 用户的答案数组
+							for (var j = 0; j < userAnswerArr.length; j++) { // 先显示错误答案
+								if(this.currentQuestion.rightOption.indexOf(userAnswerArr[j])<=-1){
+									document.getElementById(userAnswerArr[j]).style.backgroundColor = 'red'
+									document.getElementById(userAnswerArr[j]).innerHTML = '错'
+								}
+							}
+							var rightArr = this.currentQuestion.rightOption.split('') // 正确答案数组
+							for (var k = 0; k < rightArr.length; k++) {
+								document.getElementById(rightArr[k]).style.backgroundColor = 'greenyellow'
+								document.getElementById(rightArr[k]).innerHTML = '对'
+							}
+						}
+					}
+				} else {
+					that.questionIndex++
+					that.getQuestion()
+				}
+			},
+			toIndex: function() {
+				this.$router.push('/')
+			},
+			chooseOption: function(val, e) {
+				if (this.isAnswered == true) {
+					this.$mui.toast('不能再次选择')
+					return
+				}
+				// console.log(e.target.children[0].children[0])
+				// var span = e.target.children[0].children[0]
+				// this.changeSpan = span
+				// span.style.backgroundColor = '#01DBE7'
+				document.getElementById(val).style.backgroundColor = '#01DBE7'
+				if(this.currentQuestion.questionTypeId==1||this.currentQuestion.questionTypeId==2){  // 单选题和判断提的情况
+					if (this.currentQuestion.rightOption == val) {
+						this.$mui.toast('正确')
+						document.getElementById(val).style.backgroundColor = 'greenyellow'
+						document.getElementById(val).innerHTML = '对'
+						this.rightCount++
+						// 答题完毕 记录数据到本地
+						this.localAnswerLog.push({questionId:this.currentQuestion.id,answer:val,isRight:true,questionTypeId:this.questionJobTypeSelectedId})
+					} else {
+						this.$mui.toast('错误')
+						document.getElementById(val).style.backgroundColor = 'red'
+						document.getElementById(val).innerHTML = '错'
+						document.getElementById(this.currentQuestion.rightOption).style.backgroundColor = 'greenyellow'
+						document.getElementById(this.currentQuestion.rightOption).innerHTML = '对'
+						this.falseCount++
+						// 答题完毕 记录数据到本地
+						this.localAnswerLog.push({questionId:this.currentQuestion.id,answer:val,isRight:false,questionTypeId:this.questionJobTypeSelectedId})
+					}
+					this.isAnswered = true
+					
+					// // 存放到浏览器本地缓存中
+					// localStorage.setItem('localAnswerLog',JSON.stringify(this.localAnswerLog));
+				}else if(this.currentQuestion.questionTypeId==3){  //  多选题的情况
+					this.tempAnswer+=val
+					var answerNum = this.currentQuestion.rightOption.length
+					if(this.tempAnswer.length<answerNum){  //  说明还有正确答案没有选择
+						
+					}else{  // 如果选完了  立即判断对错
+						var answerArr = this.tempAnswer.split('')
+						var rightNum  =  0;
+						for (var i = 0; i < answerArr.length; i++) {
+							if(this.currentQuestion.rightOption.indexOf(answerArr[i])>-1){
+								rightNum++
+							}else{
+								document.getElementById(answerArr[i]).style.backgroundColor = 'red'
+								document.getElementById(answerArr[i]).innerHTML = '错'
+							}
+						}
+						if(rightNum==answerNum){  //  多选题答案全对的情况
+							this.$mui.toast('正确')
+							this.rightCount++
+							// 答题完毕 记录数据到本地
+							this.localAnswerLog.push({questionId:this.currentQuestion.id,answer:this.tempAnswer,isRight:true,questionTypeId:this.questionJobTypeSelectedId})
+						}else{
+							this.$mui.toast('错误')
+							this.falseCount++
+							// 答题完毕 记录数据到本地
+							this.localAnswerLog.push({questionId:this.currentQuestion.id,answer:this.tempAnswer,isRight:false,questionTypeId:this.questionJobTypeSelectedId})
+						}
+						// 显示正确答案
+						var rightAnswerArr = this.currentQuestion.rightOption.split('')
+						for (var i = 0; i < rightAnswerArr.length; i++) {
+							document.getElementById(rightAnswerArr[i]).style.backgroundColor = 'greenyellow'
+							document.getElementById(rightAnswerArr[i]).innerHTML = '对'
+						}
+						this.isAnswered = true
+					}
+				}
+				// 存放到浏览器本地缓存中
+				localStorage.setItem('localAnswerLog',JSON.stringify(this.localAnswerLog));
+			},
+			resetSpanStyle: function() {
+				document.getElementById('A').style.backgroundColor = 'inherit'
+				document.getElementById('A').innerHTML = 'A'
+
+				document.getElementById('B').style.backgroundColor = 'inherit'
+				document.getElementById('B').innerHTML = 'B'
+
+				document.getElementById('C').style.backgroundColor = 'inherit'
+				document.getElementById('C').innerHTML = 'C'
+
+				document.getElementById('D').style.backgroundColor = 'inherit'
+				document.getElementById('D').innerHTML = 'D'
+			},
+			preQuestion:function(){
+				// 加深按钮背景色
+				document.getElementById('preBtn').style.opacity=0.9
+				document.getElementById('nextBtn').style.opacity=0.9
+				setTimeout(function(){
+					document.getElementById('preBtn').style.opacity=0.5
+					document.getElementById('nextBtn').style.opacity=0.5
+				},2000)
+				//
+				this.questionIndex--
+				if (this.questionIndex == 0) {
+					this.questionIndex = 1
+					this.$mui.toast('没有上一题')
+					return
+				}
+				this.isAnswered = false
+				this.resetSpanStyle()
+				this.getQuestion()
+			},
+			nextQuestion:function(){
+				// 加深按钮背景色
+				document.getElementById('preBtn').style.opacity=0.8
+				document.getElementById('nextBtn').style.opacity=0.8
+				setTimeout(function(){
+					document.getElementById('preBtn').style.opacity=0.5
+					document.getElementById('nextBtn').style.opacity=0.5
+				},2000)
+				//
+				this.questionIndex++
+				if (this.questionIndex > this.questionCount) {
+					this.questionIndex--
+					this.$mui.toast('没有下一题')
+					return
+				}
+				this.isAnswered = false
+				this.resetSpanStyle()
+				this.getQuestion()
+			}
+		},
+		mounted: function() {
+			// mui配置
+			this.$mui.init({
+				gestureConfig: {
+					tap: true, //默认为true
+					doubletap: true, //默认为false
+					longtap: true, //默认为false
+					swipe: true, //默认为true
+					drag: true, //默认为true
+					hold: false, //默认为false，不监听
+					release: false //默认为false，不监听
+				}
+			});
+			// this.$mui.previewImage();
+			var that = this
+			document.getElementById('question').addEventListener("swiperight", function() {
+				console.log('swiperight')
+				console.log(document.getElementById('options'))
+				that.preQuestion()
+			});
+			document.getElementById('question').addEventListener("swipeleft", function() {
+				console.log('swipeleft')
+				that.nextQuestion()
+			});
+			// 获取试题类型  和索引值
+			if (this.$route.query.questionIndex == 0) {
+				this.questionIndex = this.$route.query.questionIndex + 1
+			}
+			this.questionCount = this.$route.query.questionCount
+			this.questionJobTypeSelectedId = this.$route.query.questionJobTypeSelectedId
+			console.log(this.$route.query.questionJobTypeSelectedId)
+			console.log(this.questionJobTypeSelectedId)
+			this.questionJobTypeSelectedName = this.$route.query.questionJobTypeSelectedName
+			// this.getQuestion()
+			// 获取手机型号  iPhone   Android  iPad  适配安卓解决首饰问题
+			console.log(navigator.userAgent)
+			if (navigator.userAgent.indexOf('Android') > -1) {
+				this.phoneModel = 'Android'
+			} else {
+				this.phoneModel = 'others'
+			}
+			// 从浏览器本地缓存中读取用户的答题记录
+			this.localAnswerLog = JSON.parse(localStorage.getItem('localAnswerLog'))||[];
+			// 本地统计错题数
+			this.falseCount = 0
+			this.rightCount = 0
+			for (var i = 0; i < this.localAnswerLog.length; i++) {
+				if(!this.localAnswerLog[i].isRight&&this.localAnswerLog[i].questionTypeId==this.questionJobTypeSelectedId){
+					this.falseCount++
+				}else if(this.localAnswerLog[i].isRight&&this.localAnswerLog[i].questionTypeId==this.questionJobTypeSelectedId){
+					this.rightCount++
+				}
+			}
+			this.questionIndex = this.falseCount + this.rightCount == 0 ? 1 : this.falseCount + this.rightCount
+			this.getQuestion()
+			//
+			
+		}
+
+	}
+</script>
+
+<style scoped="scoped">
+	/* .androidCtlBtn .leftBtn{
+		font-size: 100px;
+		border: 1px solid;
+		border-radius: 35px;
+		float: left;
+	} */
+
+
+	@import '../../static/mui/css/question.css'
+</style>
